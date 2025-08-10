5b4c49465af672c632fd929743ed7f0040a6abff
Fábio Valentim <maitabom@gmail.com>
2025-08-10 15:44:14 -0300

    feat: adicionar binding de saldo nos componentes de banner e atualizar lógica de saldo

--- src/app/components/banner-account/banner-account.component.ts
+++ src/app/components/banner-account/banner-account.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-banner-account',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './banner-account.component.css'
 })
 export class BannerAccountComponent {
-
+  saldo = input.required<number>();
 }
--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { BannerWelcomeComponent } from '../banner-welcome/banner-welcome.component';
 import { BannerAccountComponent } from '../banner-account/banner-account.component';
 
@@ -8,4 +8,6 @@ import { BannerAccountComponent } from '../banner-account/banner-account.compone
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css',
 })
-export class BannerComponent {}
+export class BannerComponent {
+  saldo = input.required<number>()
+}
