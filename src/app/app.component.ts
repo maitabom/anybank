6837342ac36e29e0155a4eb7a003a2593434b07f
Fábio Valentim <maitabom@gmail.com>
2025-08-10 15:35:06 -0300

    feat: adicionar lógica de processamento e cálculo de saldo para transações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { BannerComponent } from './components/banner/banner.component';
 import { FormNewTransactionComponent } from './components/form-new-transaction/form-new-transaction.component';
 import { Transacao } from './models/transacao';
+import { TipoTransacao } from './models/tipo_transacao';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,24 @@ import { Transacao } from './models/transacao';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  processarTransacao(transacao: Transacao) {}
+  transacoes = signal<Transacao[]>([]);
+
+  saldo = computed(() => {
+    return this.transacoes().reduce((acc, transacao) => {
+      switch (transacao.tipo) {
+        case TipoTransacao.DEPOSITO:
+          acc = acc + transacao.valor;
+          break;
+        case TipoTransacao.SAQUE:
+          acc = acc - transacao.valor;
+          break;
+      }
+
+      return acc;
+    }, 0);
+  });
+
+  processarTransacao(transacao: Transacao) {
+    this.transacoes.update((lista) => [transacao, ...lista]);
+  }
 }
