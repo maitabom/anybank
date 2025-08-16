b4fd35d5ec77da4af013de4c3485ec4ead8fbbfd
Fábio Valentim <maitabom@gmail.com>
2025-08-16 16:02:16 -0300

    feat: adicionar geração de ID único para transações usando nanoid

--- src/app/models/transacao.ts
+++ src/app/models/transacao.ts
@@ -1,6 +1,9 @@
+import { nanoid } from 'nanoid';
 import { TipoTransacao } from './tipo_transacao';
 
 export class Transacao {
+  readonly id = nanoid();
+
   constructor(
     public readonly tipo: TipoTransacao,
     public readonly valor: number
